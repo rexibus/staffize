389625eb940f0d045904f221413703eab74cf05e
RegisWeill <regis_weill@hotmail.com>
2018-07-03 12:34:45 +0200

    first validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  has_many :job_listings, dependent: :destroy
+
+  validates :title, :description, :start_date, :end_date, presence: true
+  validates :venue, :address, :zip_code, :province, :country, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :events, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+  validates :role, inclusion: { in: ["candidate", "employer"]}
+  before_validation :strip_email
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  # validates :first_name, :last_name, :email, :address, presence: true
+  # validates :zip_code, :province, :country, :position, presence: true
+
+  # if role == "employer"
+  # validates :company_name, :vat_number, :land_phone, presence: true
+  # else
+  # validates :experience, :language, presence: true
+  # end
+ private
+
+  def strip_email
+    self.email = email.strip unless email.nil?
+  end
+
 end
+
+
+
