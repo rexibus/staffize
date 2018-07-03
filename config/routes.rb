bbcc0179e25d443cbaaf08b31c7ddac38403a12d
Nicole Taylor-Buckner <nicoletaylorbuckner@gmail.com>
2018-07-03 15:17:34 +0200

    job_listing controller and job_listing index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'job_listings/index'
-  get 'job_listings/new'
+
+  resources :job_listings
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
