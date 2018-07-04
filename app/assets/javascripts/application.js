0c63be4693fed7f859bab1e2d84f54e775f79efb
Nicole Taylor-Buckner <nicoletaylorbuckner@gmail.com>
2018-07-04 18:03:21 +0200

    nav bar color changer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-var nav = document.getElementById('nav');
-
-  window.onscroll = function(){
-    if (window.PageYOffset > 100) {
-
-      nav.style.background = "#007bff";
-      nav.style.background = "0px 4px 2px grey"}
-    }
-
-    else {
-      nav.style.bacgkround = "transparent";
-      nav.style.boxShadow = "none"
-    }
-  }
