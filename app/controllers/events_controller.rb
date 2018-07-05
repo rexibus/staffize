a80107708572f0dd4dc9322212d647ed87bfc0da
RegisWeill <regis_weill@hotmail.com>
2018-07-05 15:02:38 +0200

    update joblising and event - linked to be checked

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
   def index
@@ -34,9 +34,12 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event.update(event_params)
+    redirect_to event_path(@event)
   end
 
   def destroy
