d4cdd4b30d52cf865a8e0581edcff28438d8d75c
RegisWeill <regis_weill@hotmail.com>
2018-07-06 13:07:54 +0200

    added photo updlaod in edit form and in db:seed

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '5.2.0'
 gem 'redis'
 gem 'pg_search'
 gem "geocoder"
+gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_094414) do
+ActiveRecord::Schema.define(version: 2018_07_06_094717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2018_07_03_094414) do
     t.text "experience"
     t.string "language"
     t.string "role"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
